9f0f6fa4166b61e38576154b936a5ef2c03f1f94
alveeislam <axislam6@shockers.wichita.edu>
2023-05-08 15:53:39 -0500

    Created child html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     """The home page for Learning Log"""
-    return render(request, 'learning_logs/index.html')
+    return render(request, 'index.html')
 
